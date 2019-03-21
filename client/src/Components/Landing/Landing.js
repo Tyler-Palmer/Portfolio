952e6a0f6dcacd19fe6f587dad54327746d3f526
tyler <tyler.william.palmer@gmail.com>
2019-03-21 16:09:57 -0600

    ref comparison completed, wall hit--working out solution

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -2,48 +2,72 @@ import React, { Component } from "react";
 import { withContent } from "../../Context/ContentProvider";
 
 class Landing extends Component {
-    constructor(props){
-        super(props)
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeThing: {
+                id: null,
+                ratio: 0
+            }
+        };
 
-        this.rootRef = React.createRef()
+        this.rootRef = React.createRef();
         this.singleRefs = this.props.things.reduce((acc, val) => {
-            acc[val.id] = React.createRef()
-            return acc
-        }, {})
+            acc[val.id] = {
+                ref: React.createRef(),
+                id: val.id,
+                ratio: 0
+            };
+            return acc;
+        }, {});
 
         const callback = entries => {
-            console.log(entries)
-        }
+            entries.forEach(
+                entry =>
+                    (this.singleRefs[entry.target.id].ratio =
+                        entry.intersectionRatio)
+            );
+            const activeThing = Object.values(this.singleRefs).reduce(
+                (acc, val) => (val.ratio > acc.ratio ? val : acc),
+                this.state.activeThing,
+            );
+            if(activeThing.ratio > this.state.activeThing.ratio){
+              this.setState({
+                activeThing
+              })
+            }
+        };
 
         this.observer = new IntersectionObserver(callback, {
             root: this.rootRef.current,
-            threshold: new Array(101).fill(0).map((v,i) => i * 0.01),
-        })
+            threshold: new Array(101).fill(0).map((v, i) => i * 0.01)
+        });
     }
     componentDidMount() {
         Object.values(this.singleRefs).forEach(value =>
-            this.observer.observe(value.current),
-            )
+            this.observer.observe(value.ref.current)
+        );
     }
-    
 
-  render() {
-    console.log(this.props.things);
-    return (
-      <div  className="landing-wrapper"
-            ref={this.rootRef}>
-        {this.props.things.map(thing => (
-          <div  id={thing.id} 
-                key={thing.id} 
-                className="text-card"
-                ref={this.singleRefs[thing.id]}>
-            <h1>{thing.headline.toString()}</h1>
-            <p>{thing.text.toString()}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
+    render() {
+        console.log(this.props.things);
+        return (
+            <div className="landing-wrapper" ref={this.rootRef}>
+                {this.props.things.map(thing => (
+                    <div
+                        id={thing.id}
+                        key={thing.id}
+                        className="text-card"
+                        ref={this.singleRefs[thing.id].ref}
+                    >
+                        <h1>{thing.headline.toString()}</h1>
+                        <p>{thing.text.toString()}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
+
 
 export default withContent(Landing);
