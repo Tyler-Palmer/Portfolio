82c02fe0ecdc40960334d9bb00f6c726fd886432
tyler <tyler.william.palmer@gmail.com>
2019-04-26 15:05:25 -0600

    resume update

--- client/src/Context/ContentProvider.js
+++ client/src/Context/ContentProvider.js
@@ -145,7 +145,7 @@ class ContentProvider extends Component {
             headline: "Contact",
             text: "I would love to connect, drop me a line on the contact form or reach out via any of the channels below.",
             text2: "This website was written in React.",
-            resume: "https://drive.google.com/open?id=1dKQHrNnJ54o81HhzxjyKFPpJCRf2Xsi0"
+            resume: "https://drive.google.com/open?id=1D4koaAJKXYi__3KAlgmFwprbabnQB5GU"
         }
         // {
         //     id: "e",
